78416a5709c69fd88ac4b33f8cd88c7b8ee4742a
talstr <tal95@daum.net>
2024-02-14 16:39:29 +0900

    training ex

--- src/training01/listFrinting.java
+++ src/training01/listFrinting.java
@@ -6,7 +6,7 @@ public class listFrinting {
 		String item1 = "한우 꽃등심";
 		int price1 = 112500;
 		
-		String item2 = "참치 선물팩";
+		String item2 = "참치 선물";
 		int price2 = 25500;
 		
 		String item3 = "맥스봉 한팩";
